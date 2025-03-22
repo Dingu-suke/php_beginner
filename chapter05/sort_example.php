ad454c06f7db7ff6ed7d88ab66b65b3c44cfc708
Dingu-suke <e2310000rubik@gmail.com>
2025-03-22 17:42:54 +0900

    配列の操作を学習

---
+++ chapter05/sort_example.php
@@ -0,0 +1,21 @@
+<?php
+$numbers = [5, 3, 8, 1, 2, 7, 4, 6];
+echo json_encode($numbers) . PHP_EOL;
+sort($numbers);
+echo json_encode($numbers) . PHP_EOL;
+
+echo '----------------------' . PHP_EOL;
+
+$numbers = [5, 3, 8, 1, 2, 7, 4, 6];
+
+for ($i = 0; $i < count($numbers); $i++) {
+  for ($j = 0; $j < count($numbers) - 1; $j++) {
+    if ($numbers[$j] > $numbers[$j + 1]) {
+      $tmp_number = $numbers[$j];
+      $numbers[$j] = $numbers[$j + 1];
+      $numbers[$j + 1] = $tmp_number;
+    }
+  }
+}
+
+echo json_encode($numbers) . PHP_EOL;
